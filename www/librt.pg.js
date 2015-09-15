57888710c0274c951d31aef65eb8994115bb169b
unknown <darce@I120028.enerminds.local>
2015-09-15 15:05:32 -0300

    Primer commit

---
+++ www/librt.pg.js
@@ -0,0 +1,294 @@
+//INFO: quickly prototype/deploy apps using phonegap & loading from url/sdcard
+enAppMovil= true;
+GLOBAL= this.GLOBAL || this;
+CfgUser= GLOBAL.CfgUser || "XxxUser";
+CfgPass= GLOBAL.CfgPass || "XxxPass";
+
+//S: base
+function ensureInit(k,v,scope) { //D: ensure k exists in scope initializing with "v" if it didn't
+ if (!(k in scope)) { scope[k]= v; }
+ return scope[k];
+}
+CFGLIB= ensureInit("CFGLIB",{},this);
+
+function str(x) {
+ var r;
+ try { r= JSON.stringify(x); }
+ catch (ex) { //A: json fails, must be circular
+  var t= typeof(x)
+  r="str_r('"+typeof(x)+"',{";
+  for (var i in x) { r+="'"+i+"': '"+x[i]+"', " }
+  r+="});"
+ }
+ return r;
+}
+
+function evalm(src,failSilently) {
+ console.log("ESTOY EN EVALM ");
+ logm("DBG",9,"EVALM",src);
+ var r;
+ try { r = window.eval(src); logm("DBG",9,"EVALM",[r,src]); }
+ catch (ex) {
+  console.log("ERROR EN EVALM " + src);
+  logm("ERR",failSilently ? 9 : 0,"EVALM",[ex.message,src]);
+  if (!failSilently) { throw(ex); }
+ }
+ return r;
+}
+
+function strToBin(d) {
+  var dataBuf = new ArrayBuffer(d.length);
+  var dataView = new Int8Array(dataBuf);
+  for (var i=0; i < d.length; i++) { dataView[i] = d.charCodeAt(i) & 0xff; }
+ return dataBuf;
+}
+
+//S: log
+CFGLIB.loglvlmax=9;
+function logm(t,l,msg,o) {
+ if (l<=CFGLIB.loglvlmax) {
+  alert(["LOG",t,l,msg,str(o)].join(":"));
+ }
+}
+
+//S: defaults
+function onFail(d) { logm("ERR",1,"ON FAIL",d); }
+function nullf() {}
+
+//S: files
+function getFile(path,fmt,cbok,cbfail) {
+ cbfail=cbfail ||onFail;
+ function read(file) {
+  var reader = new FileReader();
+  reader.onloadend = function(evt) {
+   logm("DBG",8,"getFile onloadend",{path: path, result: evt.target.result});
+   cbok(evt.target.result);
+  };
+  if (fmt=="url") { reader.readAsDataURL(file); }
+  else if (fmt=="bin") { reader.readAsBinaryString(file); }
+  else if (fmt=="array") { reader.readAsArrayBuffer(file); }
+  else { reader.readAsText(file); }
+ };
+
+ var onGotFile= function (file) { read(file); }
+
+ var onGotFileEntry= function (fileEntry) { fileEntry.file(onGotFile,cbfail); }
+
+ var onGotFs= function (fileSystem) {
+  fileSystem.root.getFile(path, {create: false}, onGotFileEntry, cbfail);
+ }
+
+ logm("DBG",8,"getFile",{path: path});
+ window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onGotFs, cbfail);
+}
+
+function getFileMeta(path,cbok,cbfail) {
+ cbfail=cbfail ||onFail;
+ var onGotFileEntry= function (fileEntry) { fileEntry.getMetadata(cbok,cbfail); }
+ var onGotFs= function (fileSystem) {
+  fileSystem.root.getFile(path, {create: false}, onGotFileEntry, cbfail);
+ }
+ logm("DBG",8,"getFile",{path: path});
+ window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onGotFs, cbfail);
+}
+
+function keysFile(dirPath,cb) {
+ window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fsSuccess, onFail);
+
+ function fsSuccess(fs) {
+  logm("DBG",9,"keysFile gotfs",[dirPath,fs.root]); try {
+   if (dirPath) { fs.root.fullPath= dirPath; } //A: cd //XXX: NO usar O restaurar, PERSISTE PARA OTRAS LLAMADAS!!!
+   var directoryReader = fs.root.createReader()
+   directoryReader.readEntries(cb,cb);
+  } catch (ex) { logm("ERR",7,"keysFile gotfs",[dirPath,ex.message]); }
+ }
+}
+
+function setFile(path,data,cbok,cbfail) {
+ cbfail=cbfail || onFail;
+
+  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, onFail);
+
+  function gotFS(fileSystem) {
+  logm("DBG",9,"setFile gotfs",[path]); try {
+  fileSystem.root.getFile(path, {create: true, exclusive: false}, gotFileEntry, cbfail);
+  } catch (ex) { logm("ERR",7,"setFile gotfs",[path,ex.message]); }
+ }
+
+  function gotFileEntry(fileEntry) {
+  logm("DBG",9,"setFile gotentry",[path]); try {
+  fileEntry.createWriter(gotFileWriter, cbfail);
+  } catch (ex) { logm("ERR",7,"setFile gotentry",[path,ex.message]); }
+  }
+
+  function gotFileWriter(writer) {
+  logm("DBG",9,"setFile write",[path]); try {
+   writer.onwriteend = function(evt) {
+     writer.onwriteend = cbok;
+     writer.write(data);
+   };
+   writer.truncate(0);
+  } catch (ex) { logm("ERR",7,"setFile write",[path,ex.message]); }
+  }
+}
+
+function setFileBin(path,data,cbok,cbfail) { setFile(path,strToBin(data),cbok,cbfail); }
+
+function setFileDir(path,cbok,cbfail) {
+ cbfail=cbfail ||onFail;
+ var parts= path.split("/");
+ var i= 0;
+
+ window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onRequestFileSystemSuccess, cbfail);
+
+ function onRequestFileSystemSuccess(fileSystem) {
+  if (parts.length==0) { cbok(fileSystem.root); }
+  else {	createPart(fileSystem.root) }
+ }
+
+ function createPart(pdir) {
+  var p= parts[i]; i++;
+ pdir.getDirectory(p, {create: true, exclusive: false}, i<parts.length ? createPart : cbok,cbfail);
+ }
+}
+
+borrarTodo_dir= function (dirPath,quiereSinPedirConfirmacion,cb) {
+ var gotDir= function (dirEntry) { try { ///XXX: separar de UI
+  var uc= quiereSinPedirConfirmacion ? "s" : prompt("esta seguro que desea eliminar '"+dirEntry.name+"'?");
+  if (uc=="s") {
+   dirEntry.removeRecursively(function () { quiereSinPedirConfirmacion || alert('los archivos han sido eliminados'); cb(); },onFail);
+  }
+  else {
+   alert("NO se eliminaran los archivos");
+  }
+ } catch (ex) { logm("ERR",7,"borrarTodo_dir cuandoTengaDir",[dirPath,ex.message]); }}
+
+ var gotFs= function (fs) { try {
+  logm("DBG",7,"borrarTodo_dir fs "+dirPath+" vs "+fs.root.fullPath);
+  fs.root.fullPath="";
+  fs.root.getDirectory(dirPath,{create: false}, gotDir,onFailAlert);
+ } catch (ex) { logm("ERR",1,"borrarTodo_dir fs",[dirPath,ex.message]); }}
+
+ window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFs, onFail);
+}
+
+//S: http
+function getHttp(url,reqdata,cbok,cbfail) {
+
+ console.log("EN getHTTP " +url+" "+reqdata);
+
+ cbfail=cbfail || onFail;
+ logm("DBG",8,"getHttp",{url: url, req: reqdata});
+
+ var userPass= Cfg.User + ":" + Cfg.Pass + ":" + "movil";
+
+ $.ajax({ url: url, data: reqdata,
+  cache: false,
+  dataType: 'text', //A: don't eval or process data
+  headers: { "Authorization": "Basic " + btoa(userPass ) },
+  beforeSend: function (jqXHR, settings) { //A: for binary downloads
+   jqXHR.overrideMimeType('text/plain; charset=x-user-defined');
+ },
+  success: function(resdata){
+   logm("DBG",8,"getHttp",{url: url, len: reqdata.length, req: reqdata, res: resdata});
+   cbok(resdata);
+  },
+  error: function (){ cbfail(reqdata);}
+ });
+}
+
+CFGLIB.pathToLib="pm/pg/";
+CFGLIB.pathDfltInLib="a/";
+
+function evalFile(name,failSilently,cbok,cbfail) {
+ console.log("EVAL FILE de " + name);
+ getFile(CFGLIB.pathToLib+name,"txt",function (srce) { try {
+  var src= encriptar_r(srce,SRC_KEY);
+  var r= evalm(src+' //# sourceURL='+name,failSilently);
+  cbok(r);
+ } catch (ex) { logm("ERR",1,"evalFile "+str(ex)); }},cbfail);
+}
+
+function evalFileOrDflt(name,failSilently,cbok,cbfail) {
+ var s0= function () { evalFile(name,failSilently,cbok,s1f); }
+ var s1f= function () { console.log("EN S1F AAAA  eval de : " +CFGLIB.pathDfltInLib+name);evalFile(CFGLIB.pathDfltInLib+name,failSilently,cbok,cbfail); }
+ s0();
+}
+
+function getHttpToDflt(fname,url,cbok,cbfail) {
+ console.log("EN GETHTTPTODLF " + fname +" URL   "+url);
+ getHttp(url,{},function (d) { try {
+  var de= encriptar(d,SRC_KEY);
+  setFile(CFGLIB.pathToLib+CFGLIB.pathDfltInLib+fname,de,cbok,cbok);
+ } catch (ex) { logm("ERR",1,"getHttpToDflt setFile "+str(ex))}},cbfail);
+}
+
+function evalUpdated(name,cbok,cbfail) {
+ var s0= function () { getHttpToDflt(name,CFGLIB.cfgurl+name,s1,s1); }
+ var s1= function () { evalFileOrDflt(name,false,cbok,cbfail); }
+ s0();
+}
+
+//S: init
+CFG_APPURL_DFLT= 'https://192.168.184.187:8443/app';
+CFGLIB.appUrl= CFG_APPURL_DFLT;
+SRC_KEY= "18273hjsjacjhq83qq3dhsjdhdy38znddj";
+function runApp() { //XXX:generalizar usando evalUpdated
+ console.log("RUN APP "+CFGLIB.appUrl);
+ logm("DBG",1,"RUN APP "+ser_json(Cfg)+" "+ser_json(CFGLIB));
+ var s0= function () {
+    getHttpToDflt('app.js',CFGLIB.appUrl,s1,s1);
+   }
+
+ var s1= function () {
+   evalFile(CFGLIB.pathDfltInLib+'app.js',false,nullf,function (err) {
+      alert("Error iniciando paso 2, ingresó los datos correctos? ("+str(err)+")");
+      LibAppStarted= false;
+      rtInit();
+      }
+     );
+  }
+ setFileDir(CFGLIB.pathToLib+CFGLIB.pathDfltInLib,s0,onFailAlert);
+}
+
+ensureInit("LibAppStarted",false,this);
+ensureInit("Cfg",false,this);
+function rtInit() {
+ if (LibAppStarted) { return true; } LibAppStarted= true;
+ CFGLIB.loglvlmax=0;
+ //D: pantalla inicial ofreciendo Run, Run con debug (alerts) y bajarse la app
+ var con= $('#con'); con.html('');
+ var form= $('<div style="font-size: 2em; text-align: center;"/>'); con.append(form);
+ var iusr=$('<input placeholder="usuario">');
+ var ipass=$('<input placeholder="clave">');
+ var iversion=$('<input placeholder="version">');
+ var bgo=$('<button>Iniciar</buton>');
+ var bgx=$('<button>Salir</buton>');
+ var bgc=$('<a href="#">(borrar datos locales)</a>');
+ form.append(iusr).append("<br>");
+ form.append(ipass).append("<br>");
+ form.append(iversion).append("<br>");
+ form.append(bgo).append("<br>");
+ form.append(bgx).append("<br><br><br>");
+ form.append(bgc);
+
+ bgo.off('click').on('click',function () { try {
+  alert("Iniciando");
+  CFGLIB.appUrl= CFG_APPURL_DFLT;
+  CFGLIB.loglvlmax= 0;
+  Cfg={};
+  Cfg.User= iusr.val(); Cfg.Pass= ipass.val(); var iv= Cfg.VersionStr= iversion.val();
+  var m= /([^:]*):?([^:]*):?(\S*)/.exec(iv);
+  if (m[3]) { CFGLIB.appUrl= m[3]+'/js' }
+  var md;
+  if (md= /d(\d?)/.exec(m[2])) { CFGLIB.loglvlmax= parseInt(md[1])||9; }
+  CFGLIB.appUrl+= m[1];
+  //XXX:SEC: cambiar PathToLib segun version para que no se pueda bajar una version de un host y acceder a los datos de otra? relacion con encriptar datos bajados?
+  //alert("Cfg "+ser_json(CFGLIB));
+  runApp(); //XXX: que hacemos si no se pudo iniciar app? hay que volver aca :)
+ } catch (ex) { alert("ERROR "+ex.message+" "+str(ex)) } });
+
+ bgx.off('click').on('click',function () { navigator.app.exitApp(); })
+ bgc.off('click').on('click',function () { borrarTodo_dir(CFGLIB.pathToLib,true,function () { alert("Los archivos locales han sido eliminados"); }); });
+}
+document.addEventListener("deviceready", rtInit, false);
---
+++ www/librt.web.js
@@ -0,0 +1,334 @@
+CACHE_LOCALSTORAGE= true;
+DBG_DATA= true;
+
+GLOBAL= this.GLOBAL || this;
+GLOBAL.DBG= GLOBAL.DBG || {};
+DBG.run= false;
+
+//****************************************************************************
+//S: UTIL/base, XXX:mover a librt!
+//XXX:logm si no existe...
+enAppMovil= enAppMovil || window.CFGLIB!=null;
+
+dbgUi= function (quiereOn) {
+    if (quiereOn) { if (enAppMovil) { CFGLIB.loglvlmax= 5; alertNative= alertNative || alert;
+        alert= function (m) { m.match(/:ERR:/) ? xalert(m) : showMsg(m); }
+    } }
+    else { if (enAppMovil) { CFGLIB.loglvlmax= 1; } }
+}
+
+window.onerror = function myErrorHandler(errorMsg,url,lineNumber,col,error) {
+    if (typeof(errorMsg)=="object") { //A: es un evento, ej. en phonegap
+        var e= errorMsg; errorMsg= str(e);
+    }
+    alert("ERROR [g] "+errorMsg+" "+url+" "+lineNumber+" "+ error);
+}
+
+function showMsg(msg) { alert(msg); }  //XXX: Cambiar por una función con el estilo de la UI
+function raiseError(msg) { showMsg(msg); }   //XXX: Cambiar por una función que guarde en el log y muestre un cartel (¿Usar Log de lib.js?)
+function fnop() {}; //D: NOP
+
+toJs= function (v) { return v; } //D: compatibilidad con rt_java
+logm= GLOBAL.logm || function(t,lvl,msg,o) { //D: usar SOLO esta funcion de log (t es DBG, NFO o ERR ; lvl es 0 para importantisimo y 9 para irrelevante, o es un objeto que se serializa (ej. diccionario)
+    console.log("LOG:"+t+":"+lvl+":"+msg+":"+(o ? ser_json(o) : ""));
+}
+
+//****************************************************************************
+//S: UTIL/LOAD/MODULES
+var Cache= { LibEval: {} };
+var LibWaiting= {};
+function libRequire(file,urlBase,url,cb) { //D: load js modules, call BEFORE using, asume async loading -> call dependent code from libStart that is always called AFTER loading all previously required modules
+console.log("LIBREQUIRE");
+    url= url || (urlBase+file);
+    var needsEval= true;
+    function processData(response) {
+        var d;
+        var raw = response ? ((typeof(response)=="string") ? response :response.responseText) : "";
+        DBG.run && alert("DBG libRequire loaded file='"+file+"' from url='"+url+"'");
+        Cache.LibEval[url]= raw || ""; //XXX: cache handling
+        if (needsEval) {
+            try { d= eval(raw); }
+            catch (ex) { alert("[LRQ] Error evaluando file='"+file+"' "+ex.message+" "+ex.fileName+" "+ex.lineNumber); }
+        }
+        if (cb) { cb(d,raw,url); }
+        delete LibWaiting[url]; //A: checked in a setTimeout loop
+        DBG.run && alert("DBG libRequire DONE file='"+file+"' from url='"+url+"'");
+    }
+
+    LibWaiting[url]= url;
+    if (Cache.LibEval[url])
+    {
+        processData(Cache.LibEval[url])
+    }
+    else {
+            if (enAppMovil) { //A: mobile, para que funcione offline Y online
+                DBG.run && alert("DBG:libRequire load with app");
+              var s0= function () { DBG.run && alert("DBG:libRequire getHttpOrDflt file='"+file+"' url='"+url+"'");
+                    getHttpToDflt(file,url,s1,s1);
+                }
+              var s1= function () { DBG.run && alert("DBG:libRequire evalFileOrDflt file='"+file+"' url='"+url+"'");
+                    evalFileOrDflt(file,false,processData);
+                    setTimeout(processData,1000); //XXX:FIX in mobile app, callback is not being called :P
+                };
+                needsEval= false;
+              s0();
+            }
+            else if (window.location.protocol=="file:") {
+                DBG.run && alert("DBG:libRequire load with script");
+                var es= document.createElement("script");
+                es.onload= function () { processData(es.innerHTML+"");};
+                es.setAttribute("id",url);
+                es.setAttribute("src",url);
+                document.body.appendChild(es);
+            }
+            else {
+                DBG.run && alert("DBG:libRequire load with ajax");
+                $.ajax({
+                    url: url,
+                    crossDomain: true,
+                    cache: false,
+                    method: 'GET',
+                    params: {},
+                    success: processData,
+                    failure: function (response, opts) {
+                        alert("ERROR: [LRW] libRequire file='"+file+"' "+response.responseText);
+                        if (cb) { cb(); }
+                    }
+                });
+            }
+    }
+    //XXX: handle error!
+    //XXX: ¿Si se quedó sin memoria? ¿Ofrecer borrar una zona?
+}
+
+var LibStartChk;
+function libStart(cb,msg) { //D: call start function after all libRequire have been satisfied
+    LibStartChk= (new Date()).getMilliseconds()+10000;
+    if (Object.keys(LibWaiting).length==0) { //A: no hay pendientes para cargar
+        showMsg("[RS] "+(msg || "Listo, iniciando!"));
+        if (cb) { try { cb(); } catch (ex) { alert("[RS] ERROR iniciando "+ex.message+" "+ex.stack); } }
+    }
+    else { //A: todavia esperamos algun require
+        console.log("DBG libStart waiting for "+Object.keys(LibWaiting));
+        setTimeout(function () { libStart(cb); },200 );
+        if ((new Date()).getMilliseconds()>LibStartChk) {
+            alert("ERROR [RW] no puedo cargar "+Object.keys(LibWaiting));
+            LibStartChk= (new Date()).getMilliseconds()+10000;
+        }
+        //A: probamos de nuevo en 200ms
+    }
+}
+
+
+toJs= function (v) { return v; } //D: compatibilidad con rt_java
+
+truncate= function (num,decs) {
+    var x= Math.pow(10,decs);
+    return Math.floor(num*x)/x;
+}
+
+logm= GLOBAL.logm || function(t,lvl,msg,o) { //D: usar SOLO esta funcion de log (t es DBG, NFO o ERR ; lvl es 0 para importantisimo y 9 para irrelevante, o es un objeto que se serializa (ej. diccionario)
+    console.log("LOG:"+t+":"+lvl+":"+msg+":"+(o ? ser_json(o) : ""));
+}
+
+logmAndThrow= function (t,lvl,msg,o) {
+    logm(t,lvl,msg,o);
+    throw({message: msg, data: o});
+}
+
+logmex= function(t,lvl,msg,o,ex) {
+    var es= (typeof(ex)=="string" && ex) || (ex.message && (ex.message + (ex.data ? (" "+ser_json(ex.data)) : "")) || ex.getMessage()|| "").replace(/\r?\n/g," ");
+    if (ex.stack) { es+= " "+ex.stack.replace(/\r?\n/g," > ");}
+    else {
+        if (ex.fileName) { es+= " "+ex.fileName;}
+        if (ex.lineNumber) { es+= ":"+ex.lineNuber;}
+    }
+    logm(t,lvl,msg+" EXCEPTION "+es,o);
+}
+
+onFailAlert= function (err) { alert("ERROR: "+str(err)); }
+
+ser_json= function (o,wantsPretty) {
+    var s;
+    if (o!=null) {
+        try { s= JSON.stringify(o,null,wantsPretty ? 2 : null); }
+        catch (ex) { s=o+""; }
+    }
+    else {
+        s="null";
+    }
+    return s;
+}
+
+ser_json_r= function (s) {
+    try {
+        return JSON.parse(s);
+    }
+    catch (ex) {
+        logmex("ERR",5,"SER PARSE JSON",s,ex);
+        throw(ex);
+    }
+}
+
+ser= ser_json; //DFLT
+
+ser_planoOproto= function (ox,serFun,wantsPretty) { //U: para NO encodear strings, usa el primer caracter para distinguir
+    var o= toJs(ox)
+    return ((typeof(o)=="string") ? ("\t"+o) : (" "+serFun(o,wantsPretty)));
+}
+
+ser_planoOproto_r= function (s,serFun_r) {
+    return (s && s.length>0) ?
+        s.charAt(0)=="\t" ? s.substr(1) : serFun_r(s.substr(1)) :
+        null;
+}
+
+ser_planoOjson= function (o, wantsPretty) { return ser_planoOproto(o,ser_json,wantsPretty); }
+ser_planoOjson_r= function (s) { return ser_planoOproto_r(s,ser_json_r); }
+
+
+load= function (l) { console.log("REVISAR QUE "+l+" ESTE CARGADO") };
+
+cxAjax= function (cmd,args,pipe,cb) {
+    pipe= pipe || 'Json';
+    req= args; req.cmd= cmd;
+    console.log("LOG:DBG:1:CX cxAjaxSync "+req);
+    var r= null;
+    $.ajax({
+        type: 'GET',
+        url: '/app/cx',
+        data: { pipe: pipe, json: JSON.stringify(req) },
+        dataType: 'text',
+        timeout: 300000,
+        async: cb!=null,
+        success: function(data){
+            DBG_DATA && console.log("DATA: "+data);
+            r= data;
+            cb && cb(pipe=='Json' ? JSON.parse(data) : data);
+        },
+        error: function(xhr, status, error){ showMsg('Ajax error!' + status + " "+xhr.responseText+ " "+JSON.stringify(req) ) }
+    });
+    return pipe=='Json' ? JSON.parse(r) : r;
+}
+
+//XXX: estas pueden ir en lib.js
+promedio= function (v0,v1) { return v0+(v1-v0)/2; }
+clonar= function (o) { var r= {}; for (var k in o) { r[k]= o[k] }; return r; }
+
+CfgDbKey = GLOBAL.CfgDbKey || '3sUns3Cr3t0!';
+encriptar= function (data,key) { try {
+     var datae= sjcl.encrypt(key || CfgDbKey,btoa(encodeURIComponent(ser_planoOjson(data))));
+    return datae;
+} catch(ex) { logmex("ERR",1,"ENCRIPTAR",data,ex); throw(ex) } }
+
+encriptar_r= function (data,key) { try {
+     return ser_planoOjson_r(decodeURIComponent(atob(sjcl.decrypt(key || CfgDbKey,data))));
+} catch(ex) { logmex("ERR",1,"ENCRIPTAR R",data,ex); throw(ex) } }
+
+runBg= L.Util.requestAnimFrame;
+
+funcionConCache_a_archivos= function (nombre,funcion,cbIdx,funcionClavePara,cacheCntMax,prefijoNombreArchivo) { //D: envuelve una funcion con cache, si esta en movil usa archivos encriptados para cuando este offline
+    prefijoNombreArchivo= prefijoNombreArchivo || "";
+    var cache= nuevo_cache_recienUsados(nombre,cacheCntMax);
+    var funcionConCacheArchivos= funcion; //A: DFLT
+    if (!enAppMovil && CACHE_LOCALSTORAGE) {
+        GLOBAL.CFGLIB={};
+        GLOBAL.CFGLIB.pathToLib="Em_Map_";
+        getFile= function (p,t,cbok,cbf) { var v= localStorage[p]; setTimeout(function () { v ? cbok(v) : cbf()},100); }
+        setFile= function (p,v,cbok,cbf) { localStorage[p]= v; setTimeout(cbok,100); }
+        setFileBin= function (p,v,cbok,cbf) { localStorage[p]= v; setTimeout(cbok,100); }
+        setFileDir= function (p,cbok,cbf) { setTimeout(cbok,100); }
+        borrarTodo_dir= function (p,quiereSinPedirConfirmacion,cb) {
+            for (var i=0; i<localStorage.length; i++) { var k= localStorage.key(i);
+                if (k.substr(0,p.length)==p) { localStorage.removeItem(k); }
+            }
+            setTimeout(cb,10);
+        };
+		
+		removeTemporaryNoteFromLocalStorage = function(elementId, t, cbok, cbf) {
+		for (var i = 0; i < localStorage.length; i++) {
+            var k = localStorage.key(i);
+			var regex = new RegExp("note." + elementId);
+			if (regex.test(k)) {
+				logm("DBG", 1, "SE ELIMINO LA NOTA TEMPORARIA PARA EL elementId : " + elementId);
+                localStorage.removeItem(k);
+            }
+        }
+        setTimeout(cb, 10);
+		};
+		
+		changeNoteInLocalStorage = function(elementId, newNote, t, cbok, cbf) {
+			for (var i = 0; i < localStorage.length; i++) {
+				var k = localStorage.key(i);
+				var regex = new RegExp("\D*(note_5f)+.*(" + elementId + ")+");
+				newNote_encriptado = encriptar([newNote]);
+				if (regex.test(k)) {
+					localStorage[k] = ser_planoOjson(newNote_encriptado);
+					logm("DBG", 1, "NOTA MODIFICADA: " + JSON.stringify(newNote));
+				}
+			}
+			setTimeout(cb, 10);
+		};
+		
+    }
+    if (enAppMovil || CACHE_LOCALSTORAGE) {
+        var cacheArchivos= nuevo_cache_envuelto(nuevo_cache_archivosMovil(nombre+"_archivos",prefijoNombreArchivo),encriptar,encriptar_r);
+        funcionConCacheArchivos= funcionConCache_a(funcion,cbIdx,cacheArchivos,funcionClavePara);
+    }
+    var funcionr= funcionConCache_a(funcionConCacheArchivos,cbIdx,cache,funcionClavePara);
+    funcionr.cache= cache;
+    funcionr.cacheArchivos= cacheArchivos;
+    return funcionr;
+}
+
+
+rtInfo= function () { try {
+    var s= "AV: "+navigator.appVersion;
+    if (window.device) {
+        s+= "\nD:"+device.uuid+" "+device.model+" "+device.platform+" "+device.version+" "+device.name;
+    }
+    else { s+= "\nD:noinfo"; }
+    return s;
+} catch(ex) { logmex("ERR",1,"RT INFO",null,ex)} }
+
+//***************************************************************************
+//S: UI
+msgInit= function () {
+    if (this.msgDiv_) { return; }
+    msgDiv_= document.createElement("DIV");
+    msgDiv_.style.position= "fixed";
+    msgDiv_.style.top= "0px";
+    msgDiv_.style.left= "0px";
+    msgDiv_.style.width= "30em";
+    msgDiv_.style.minHeight= "5em";
+    msgDiv_.style.background= "black";
+    msgDiv_.style.color= "red";
+    msgDiv_.style.zIndex= 9999;
+    msgDiv_.style.border= "1px dotted gray";
+    msgDiv_.style.padding= "0.5em";
+    msgDiv_.style.wordWrap= "break-word";
+    msgDiv_.style.visibility= "hidden";
+    msgDiv_.style.overflow= "scroll";
+    document.body.appendChild(msgDiv_);
+}
+
+MsgSz= 500;
+_msgTimeout= null;
+showMsg= function (s) {
+    msgInit();
+    var s0= msgDiv_.innerHTML+s+"<br><br>";
+    msgDiv_.innerHTML= s0.length > MsgSz ? s0.substr(s0.length-MsgSz) : s0;
+    clearTimeout(_msgTimeout);
+    _msgTimeout= setTimeout(function () { msgDiv_.style.visibility="hidden"; msgDiv_.innerHTML="" }, 5000);
+    msgDiv_.style.visibility="visible";
+}
+
+
+var appHost= (enAppMovil && CFGLIB.appUrl) ? CFGLIB.appUrl.replace(/[^\/]+$/,"") : CFG_APPURL_DFLT;
+//var appHost='https://192.168.10.4:8443/app/';
+var host= location.host ? "//"+location.host+"/app/" : appHost; //XXX: hack for phonegap, move inside libRequire, implement differently in each runtime
+DBG.run && alert("HOST '"+host+"'");
+
+if (document.body) { document.body.innerHTML="CARGANDO de "+appHost+"..."; } //XXX: generalzar, pantalla de inicio
+
+
